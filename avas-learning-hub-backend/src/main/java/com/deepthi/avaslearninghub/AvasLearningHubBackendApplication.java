e1bdcf9e7ec6e9474ac5daba1f1318c6cf98904e
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-02-21 20:50:33 -0600

    Initial Spring Boot setup with MySQL configuration

---
+++ avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/AvasLearningHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.deepthi.avaslearninghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvasLearningHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvasLearningHubBackendApplication.class, args);
+	}
+
+}
---
+++ avas-learning-hub-backend/src/test/java/com/deepthi/avaslearninghub/AvasLearningHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepthi.avaslearninghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvasLearningHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
