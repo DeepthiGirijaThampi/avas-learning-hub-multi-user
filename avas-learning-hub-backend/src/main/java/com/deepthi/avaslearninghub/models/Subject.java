ca2f709db4fed2b89abf06274dbcc7bcbc6d720f
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-02-26 20:51:02 -0600

    Updating Subjects.java 001

--- avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/models/Subject.java
+++ avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/models/Subject.java
@@ -1,5 +1,23 @@
 package com.deepthi.avaslearninghub.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "subjects")
 public class Subject {
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
+     @Column(nullable = false,length = 80)
+     private String name;
+     @Column(length = 30)
+     private String color;
+     @Column(length=50)
+     private String icon;
+
+
+
+
 
 }
