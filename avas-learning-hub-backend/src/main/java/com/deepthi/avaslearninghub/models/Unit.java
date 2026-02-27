626780cd984b77a7ac16b2c9ceb7a9e4b2bfda2d
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-02-27 10:05:28 -0600

    Updated Units models 001

--- avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/models/Unit.java
+++ avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/models/Unit.java
@@ -1,9 +1,6 @@
 package com.deepthi.avaslearninghub.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
@@ -11,8 +8,9 @@ import java.time.LocalDateTime;
 @Table(name = "units")
 public class Unit {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column
+    @Column(name = "subject_id", nullable = false)
     private Long subjectId;
     @Column(nullable = false, length = 80)
     private String title;
@@ -25,7 +23,64 @@ public class Unit {
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    public Unit() {
+    }
 
+    public Unit(Long subjectId, String title, boolean isCompleted, LocalDateTime completedAt, Integer sortOrder, LocalDateTime createdAt) {
+        this.subjectId = subjectId;
+        this.title = title;
+        this.isCompleted = isCompleted;
+        this.completedAt = completedAt;
+        this.sortOrder = sortOrder;
+        this.createdAt = createdAt;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public Long getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(Long subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    public Integer getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(Integer sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 
 }
