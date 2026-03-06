f23220e449cd1495cbffbafee99966d6d38d83e7
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-03-06 11:47:42 -0600

    Added dependencies for Jwt, created dto-RegisterRequest, AuthResponse, LoginRequest

--- avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/repositories/UserRepository.java
+++ avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/repositories/UserRepository.java
@@ -4,4 +4,8 @@ import com.deepthi.avaslearninghub.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
