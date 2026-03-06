314ee3dc6a5a7e4d78b3dce2d1f0f7eeecdcae1c
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-03-06 15:25:51 -0600

    Added JwtAuthenticationFilter and Updated AuthController, Service and Config

--- avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/controllers/AuthController.java
+++ avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/controllers/AuthController.java
@@ -1,4 +1,34 @@
 package com.deepthi.avaslearninghub.controllers;
 
+import com.deepthi.avaslearninghub.dto.LoginRequest;
+import com.deepthi.avaslearninghub.dto.RegisterRequest;
+import com.deepthi.avaslearninghub.services.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request){
+        authService.register(request);
+        return new ResponseEntity<>("User registerd successfully", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request){
+
+        return ResponseEntity.ok(authService.login(request));
+    }
 }
--- avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/services/AuthService.java
+++ avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/services/AuthService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthService {
 
-    @Autowired
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
