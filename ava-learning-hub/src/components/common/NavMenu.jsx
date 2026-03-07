5cc9584afa18e71c4058db98905c5032d3723ca2
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-03-07 13:22:44 -0600

    Updated NavMenu, Login

--- ava-learning-hub/src/components/common/NavMenu.jsx
+++ ava-learning-hub/src/components/common/NavMenu.jsx
@@ -1,8 +1,24 @@
-import { Link } from "react-router";
+import { Link , useNavigate} from "react-router";
 import { useState } from 'react';
 // The NavMenu component displays a responsive navigation menu
 export default function NavMenu(){
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token"); 
+  const userName = localStorage.getItem("userName");
+  const firstName = userName ? userName.split(" ")[0] : "";
+  //handler for logout
+  const handleLogout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("userName");
+  localStorage.removeItem("userEmail");
+ 
+  setMenuOpen(false);
+  navigate("/login");
+};
+
   // Toggles the menu's open/closed state  
   const toggleMenu = () => {
     setMenuOpen(!menuOpen)
@@ -17,10 +33,27 @@ export default function NavMenu(){
       <div className={`menu-links ${menuOpen ? 'show' : ''}`}>
         <Link className="link" id="links" to="/" onClick={() => setMenuOpen(false)} >Home  </Link>
         <Link className="link" id="links" to="/about" onClick={() => setMenuOpen(false)} >About  </Link>
+        
+        {token && (
+          <>
         <Link className="link" id="links" to="/profile" onClick={() => setMenuOpen(false)} >Profile  </Link>
         <Link className="link" id="links" to="/reflections" onClick={() => setMenuOpen(false)} >Reflections  </Link>
         <Link className="link" id="links" to="/subjects" onClick={() => setMenuOpen(false)} >Subjects  </Link>
+        </>
+        )}
         <Link className="link" id="links" to="/contacts" onClick={() => setMenuOpen(false)} >Contact Us  </Link>
+        {token ? (<>
+                  <span className="link"  >Hello {firstName} 👋</span>
+                  <button className="link" onClick={() => {handleLogout();
+                                                          setMenuOpen(false);
+                                                          }}>Logout</button>
+                  </>                                        
+                  ) : (
+                  <>
+                    <Link className="link" id="links" to="/login" onClick={() => setMenuOpen(false)}> Login </Link>
+                    <Link className="link" id="links" to="/register" onClick={() => setMenuOpen(false)}> Register </Link>
+                  </>
+        )}
       </div>
     </nav>
   )
--- ava-learning-hub/src/components/pages/Login.jsx
+++ ava-learning-hub/src/components/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
     email: "",
     password: ""
   });
-
+  
   const handleLogin = async (e) =>{
     e.preventDefault();
 
