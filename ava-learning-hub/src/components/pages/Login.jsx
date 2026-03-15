90475ea63d9ec036df58425908a022a29d5b9cf8
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-03-15 09:50:49 -0500

    cleaned Login.css

--- ava-learning-hub/src/components/pages/Login.jsx
+++ ava-learning-hub/src/components/pages/Login.jsx
@@ -3,34 +3,43 @@ import {useNavigate,Link} from "react-router";
 import CustomButton from "../common/CustomButton";
 import { loginUser } from "../../services/authService";
 import './auth.css';
+// Login component handles user authentication
 export default function Login() {
 
+  // Hook to navigate to another route after successful login
   const navigate = useNavigate();
-
+  // State to store email and password entered by the user
   const [formData, setFormData] = useState({
     email: "",
     password: ""
   });
-  const [error, setError] = useState("");
+  // State to display login error messages
+  const [error, setError] = useState(""); 
+
+  // Handler for login form submission
   const handleLogin = async (e) =>{
     e.preventDefault();
 
     try{
+      // Call backend login API
      const data = await loginUser(formData);
+
+     // Store authentication details in localStorage
      localStorage.setItem("token", data.token);
      localStorage.setItem("userId", data.userId);
      localStorage.setItem("userName", data.name);
      localStorage.setItem("userEmail", data.email);
 
+     // Redirect user to Subjects page after successful login
      navigate("/subjects");
-     console.log("Login successful",data);   
 
     }catch(error){
-     console.log("Login failed",error.message);
+    //Reset form feilds if login fails
      setFormData({
       email: "",
       password:""
      })
+     //display error msg for invalid credentials
      setError("Invalid email or password");
      setTimeout(()=>{
       setError("");
@@ -40,17 +49,12 @@ export default function Login() {
 
   return (
     <main className="page-container">
-    {/* <div className="floating-icons">
-      <span>📚</span>
-      <span>✏️</span>
-      <span>🎓</span>
-      <span>📖</span>
-      
-    </div> */}
+    
       <h1 className="page-heading">Login </h1>
       <p className="auth-subtitle">
         Welcome back! Continue your learning journey.
       </p>
+      {/* Login form */}
       <form onSubmit={handleLogin}>
         <input
           type="email"
@@ -74,8 +78,9 @@ export default function Login() {
         />
 
         <br/><br/>
-
+        {/* Submit button */}
         <CustomButton text="Login" type="submit"/>
+        {/* Display error message if login fails */}
         {error && <p className="form-error">{error}</p>}
       </form>
             <p style={{ textAlign: "center", marginTop: "1rem" }}>
