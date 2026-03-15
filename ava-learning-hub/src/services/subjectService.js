f653e07e8736475221119f7067fcdf8eff130746
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-03-15 12:54:36 -0500

    cleaned subjectService

--- ava-learning-hub/src/services/subjectService.js
+++ ava-learning-hub/src/services/subjectService.js
@@ -1,6 +1,7 @@
+// Base URL for all subject-related API endpoints
 const API_BASE_URL = "http://localhost:8080/api/subjects";
 
-//get subject need token in userId as params
+//gets subjects by a specific user - need token in userId as params
 export async function getSubjectsByUser(userId, token){
     const response = await fetch(`${API_BASE_URL}/by-user/${userId}`,{
         method: "GET",
@@ -18,7 +19,7 @@ export async function getSubjectsByUser(userId, token){
     return response.json();
 }
 
-//create subject need subData and token
+//creates a new subject - needs subjectData and token
 export async function createSubject(subjectData, token) {
 
     const response = await fetch(`${API_BASE_URL}`,{
@@ -37,7 +38,7 @@ export async function createSubject(subjectData, token) {
     return response.json();
 }
 
-//Delete Subject - subjectId, token
+//Delete Subject - needs subjectId, token
 export async function deleteSubject(subjectId, token) {
   const response = await fetch(`${API_BASE_URL}/${subjectId}`, {
     method: "DELETE",
@@ -53,7 +54,7 @@ export async function deleteSubject(subjectId, token) {
   return true;
 }
 
-//Update Suject
+//Update an existing Subject - needs sunjectId subjectData and tokrn as params
 export async function editSubject(subjectId,subjectData,token) {
 
     const response = await fetch(`${API_BASE_URL}/${subjectId}`,{
