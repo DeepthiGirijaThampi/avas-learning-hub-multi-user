77802ce63563db18ac3a9f8d0299c452e377a418
DeepthiGirijaThampi <deepthig01@gmail.com>
2026-03-15 19:23:08 -0500

    Cleaned Repositories

--- avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/repositories/SubjectRepository.java
+++ avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/repositories/SubjectRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+// Repository interface for managing Subject entities in the database
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
     //select * from subjects where user_id = ?
     List<Subject> findByUser_Id(Long userId);
--- avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/repositories/UnitRepository.java
+++ avas-learning-hub-backend/src/main/java/com/deepthi/avaslearninghub/repositories/UnitRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+// Repository interface for managing Unit entities in the database
 public interface UnitRepository extends JpaRepository<Unit,Long> {
     //select * from units where subject_id = ?
     List<Unit> findBySubject_Id(Long subjectId);
